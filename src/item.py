7fb3e0ac470c4dea2c0789d1cf5ca4537f9693c3
NickKrlv <Nick.krlv@yandex.ru>
2023-10-10 11:24:02 +0300

    Add repr, str

--- src/item.py
+++ src/item.py
@@ -21,6 +21,12 @@ class Item:
         self.price = price
         self.quantity = quantity
 
+    def __repr__(self):
+        return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity})"
+
+    def __str__(self):
+        return f"{self.name}"
+
     def calculate_total_price(self) -> float:
         """
         Рассчитывает общую стоимость конкретного товара в магазине.
@@ -58,12 +64,9 @@ class Item:
             for row in reader:
                 Item.all.append(Item(row['name'], row['price'], row['quantity']))
 
-
     @staticmethod
     def string_to_number(value: str) -> int:
         """
         Преобразует строковое значение в целое число.
         """
         return int(float(value))
-
-
